3363a88dd8fa1f490362e206f8782fe424063a77
umernadim <itsumernadeem@gmail.com>
2025-10-18 12:13:31 +0500

    new conditions

--- if-else/Program.cs
+++ if-else/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//if-else statement 
+//if-else statements
 
+// condition 1 ---> Voting Eligibility
 //Console.WriteLine("Enter your name");
 //var name = Console.ReadLine();
 //Console.WriteLine($"My name is {name}");
@@ -17,37 +18,110 @@
 //    Console.WriteLine("Sorry, You are not eligible to vote");
 //}
 
-//conditon 2
+//conditon 2 ---> Grade Calculator
 
-Console.WriteLine("Enter your marks");
-var marks = Convert.ToInt32(Console.ReadLine());
-if (marks >= 80 && marks <= 100){
-    Console.WriteLine("Your got A-1");
-}
-else if(marks >= 70 && marks < 80)
-{
-    Console.WriteLine("You got A grade");
-}
-else if (marks >= 60 && marks < 70)
-{
-    Console.WriteLine("You got B grade");
-}
-else if (marks >= 50 && marks < 60)
-{
-    Console.WriteLine("You got C grade");
-}
-else if (marks >= 40 && marks < 50)
-{
-    Console.WriteLine("You got D grade");
-}
-else if (marks < 40)
-{
-    Console.WriteLine("You got fail");
-}
-else
-{
-    Console.WriteLine("Please, enter valid marks");
-}
+//Console.WriteLine("Enter your marks");
+//var marks = Convert.ToInt32(Console.ReadLine());
+//if (marks >= 80 && marks <= 100){
+//    Console.WriteLine("Your got A-1");
+//}
+//else if(marks >= 70 && marks < 80)
+//{
+//    Console.WriteLine("You got A grade");
+//}
+//else if (marks >= 60 && marks < 70)
+//{
+//    Console.WriteLine("You got B grade");
+//}
+//else if (marks >= 50 && marks < 60)
+//{
+//    Console.WriteLine("You got C grade");
+//}
+//else if (marks >= 40 && marks < 50)
+//{
+//    Console.WriteLine("You got D grade");
+//}
+//else if (marks < 40)
+//{
+//    Console.WriteLine("You got fail");
+//}
+//else
+//{
+//    Console.WriteLine("Please, enter valid marks");
+//}
+
+
+//condition 3 ---> Positive or Negative number finder
+//Console.WriteLine("Enter a number");
+//var number = Convert.ToInt32(Console.ReadLine());
+
+//if (number > 0)
+//{
+//    Console.WriteLine("Number is Positive");
+//}
+//else if (number < 0)
+//{
+//    Console.WriteLine("Number is Negative");
+//}
+//else
+//{
+//    Console.WriteLine("Number is Zero");
+//}
+
+
+// condition 4 ---> Even or Odd number finder
+//Console.WriteLine("Enter a number");
+//var number = Convert.ToInt32(Console.ReadLine());
+
+//if (number % 2 == 0 )
+//{
+//    Console.WriteLine("Even");
+//}
+//else
+//{
+//    Console.WriteLine("Odd");
+//}
+
+// condition 5 ---> Find the largest of two numbers
+
+//Console.WriteLine("Enter first number");
+//var num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second number");
+//var num2 = Convert.ToInt32(Console.ReadLine());
+//if (num1 > num2)
+//{
+//    Console.WriteLine($"{num1} is greater than {num2}");
+//}
+//else if (num1 < num2)
+//{
+//    Console.WriteLine($"{num2} is greater than {num1}");
+//}
+//else if(num1 == num2)
+//{
+//    Console.WriteLine($"{num1} is equal to {num2}");
+//}
+//else
+//{
+//    Console.WriteLine("Please enter valid numbers");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
